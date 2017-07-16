77d75b3b49fe7f0a00d340ab3446d6a2e91f9933
pradeep026 <kspradeepbe@gmail.com>
2017-07-16 20:23:20 +0800

    Add HttpHandler class to handle Ajax

---
+++ src/services/HttpHandler.js
@@ -0,0 +1,27 @@
+const HostUrl =  "http://pb-api.herokuapp.com";
+import fetch from 'isomorphic-fetch'
+
+const HttpHandler = () => {
+
+  const ajax = (url) => {
+    return new Promise( (resolve, reject) =>{
+      fetch(url)
+        .then(response => response.json())
+        .then(response => {
+          resolve(response);
+        }).catch( (e) => {
+          console.log('parsing failed', e);
+          reject(e)
+       });
+    });
+  }
+
+  const findRecord = (serviceUrl, options = {}) =>{
+    const url = `${HostUrl}${serviceUrl}`;
+    return ajax(url);
+  }
+
+  return {  findRecord };
+}
+
+export default HttpHandler();
